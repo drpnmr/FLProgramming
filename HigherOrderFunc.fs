e85820f0a69d74d0dd9d002f1fdb99e94f031cac
Darya <darya.pnmr@gmail.com>
2025-03-19 12:52:40 +0300

    ЛР1, задание 2. Написать функцию решения квадратного уравенения

--- HigherOrderFunc.fs
+++ HigherOrderFunc.fs
@@ -1,2 +1,12 @@
 
-printfn "Hello World!"
+//Задание 1. Вывести Hello World
+
+printfn "Hello World"
+
+//Задание 2. Написать функцию решения квадратного уравнения
+
+let solve (a, b, c) = 
+    let D = b*b-4.*a*c in
+        (-b+sqrt(D))/(2.0*a), (-b-sqrt(D))/(2.0*a)
+
+System.Console.WriteLine(solve (1.0, 2.0, -3.0))
